c1afac3242eedb623e782c00bfe582856591e95a
jonahokot <jonahrokot2@gmail.com>
2022-09-27 15:12:02 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const path = require ('path'):
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'Search Hacker News',
+  });
+});
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(` server started on port: ${server.address().port}`);
+});
