db7538c3ff6332d73e5c621ee83de3516dfacf64
jonahokot <jonahrokot2@gmail.com>
2022-09-27 16:00:02 +0300

    first meet

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-
-const path = require ('path'):
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    title: 'Search Hacker News',
+  res.render('calculator', {
+    title: 'Calculator',
   });
 });
-
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(` server started on port: ${server.address().port}`);
